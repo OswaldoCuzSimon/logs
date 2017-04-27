b89cec41e2b2c149156dd28dd3c2e5e9dab5397e
OswaldoCuzSimon <oswaldo_cs_94@hotmail.com>
2017-04-27 17:38:42 -0500

    Se agregaron index a las tablas

--- cassandraScheme.sql
+++ cassandraScheme.sql
@@ -8,7 +8,7 @@ CREATE TABLE messages(
 	message text
 );
 CREATE TABLE httpd(
-	id_httpd timeuuid PRIMARY KEY,
+	id_httpd timeuuid,
 	ip inet,
 	userid varchar,
 	not_available varchar,
@@ -18,5 +18,16 @@ CREATE TABLE httpd(
 	protocol varchar,
 	status_code varchar,
 	size_object varchar,
-	message text
+	message text,
+	PRIMARY KEY(id_httpd)
 );
+CREATE INDEX ON httpd( userid );
+CREATE INDEX ON httpd( status_code );
+CREATE INDEX ON httpd( ip );
+CREATE INDEX ON httpd( method );
+CREATE INDEX ON httpd( datetime );
+
+
+CREATE INDEX ON messages( datetime );
+CREATE INDEX ON messages( hostname );
+CREATE INDEX ON messages( service );
